6e9d1d7fa0b2d7f080beaa7d76187469cbf3e08b
MatteoZocca <mat.zocca@stud.uniroma3.it>
2020-03-11 18:58:36 +0100

    Inserimento Scena
    Prime prove con Unity, attività svolte:
    -Inserimento di oggetti nella scena come Sfera e Capsula.
    -Sviluppo di un primo script (sperimentale) di movimento per il personaggio.

---
+++ Terrapiattisti/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // variabile velocità
+    [SerializeField] // compare nell'Inspector, anche public lo fa, con la differenza che la rende visibile ad altre classi.
+    float speed = 5;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
+
+          if(movementX != 0 || movementY != 0)
+        {
+            Vector3 vettoreMovimento = new Vector3(movementX, movementY, 0);
+            this.transform.Translate(vettoreMovimento * speed * Time.deltaTime);
+        }    
+    }
+}
