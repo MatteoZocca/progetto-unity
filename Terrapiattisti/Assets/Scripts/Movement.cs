c9136c8995529f4692ce73fcdc244da858dd500d
MatteoZocca <mat.zocca@stud.uniroma3.it>
2020-03-11 19:12:53 +0100

    Aggiunta commenti Script
    Aggiunta commenti dell'ultimo commit.

--- Terrapiattisti/Assets/Scripts/Movement.cs
+++ Terrapiattisti/Assets/Scripts/Movement.cs
@@ -9,18 +9,19 @@ public class Movement : MonoBehaviour
     float speed = 5;
     void Start()
     {
-        
+        // vuoto non faccio nulla all'avvio del gioco
     }
 
     void Update()
     {
-        float movementX = Input.GetAxis("Horizontal");
-        float movementY = Input.GetAxis("Vertical");
+        float movementX = Input.GetAxis("Horizontal"); //salvo input asse X
+        float movementY = Input.GetAxis("Vertical"); // salvo input asse Y
 
-          if(movementX != 0 || movementY != 0)
+          if(movementX != 0 || movementY != 0) // verifico se c'è stato movimento
         {
-            Vector3 vettoreMovimento = new Vector3(movementX, movementY, 0);
+            Vector3 vettoreMovimento = new Vector3(movementX, movementY, 0); // creo un nuovo vettore di movimento 
             this.transform.Translate(vettoreMovimento * speed * Time.deltaTime);
+            // traslo l'oggetto in questione per un valore di velocità * una variabile di tempo che varia a seconda del PC.
         }    
     }
 }
