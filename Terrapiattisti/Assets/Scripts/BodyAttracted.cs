e0389db033797c6bc085d9c79de97ceb61fdcae3
MatteoZocca <mat.zocca@stud.uniroma3.it>
2020-03-12 02:38:07 +0100

    Forza di Gravità
    Inserimento forza di gravità terrestre.

---
+++ Terrapiattisti/Assets/Scripts/BodyAttracted.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Questo classe descrive un elemento passivo che viene attratto da un elemento, in questo caso la terra */
+
+public class BodyAttracted : MonoBehaviour
+{
+    public Attractor attrattore;
+    
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        // freeza la rotazione dell'elemento, tiene sempre i piedi rivolti verso la terra.
+    }
+
+    
+    void Update()
+    {
+        //Chiama funzione ogni volta per tenere attaccato l'oggetto.
+        attrattore.GravityOggetto(this.transform);
+    }
+}
---
+++ Terrapiattisti/Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    // variabile velocità
+    [SerializeField] // compare nell'Inspector, anche public lo fa, con la differenza che la rende visibile ad altre classi.
+    float speed = 5;
+    Vector3 vettoreMovimento; // vettore che indica la direzione del movimento
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        // freeza la rotazione del personaggio, tiene sempre i piedi rivolti verso la terra.
+    }
+
+    void Update()
+    {
+        float movementX = Input.GetAxis("Horizontal"); //salvo input asse X
+        float movementY = Input.GetAxis("Vertical"); // salvo input asse Y
+        vettoreMovimento = new Vector3(movementX, 0, movementY); // creo un nuovo vettore di movimento 
+            this.transform.Translate(vettoreMovimento * speed * Time.deltaTime);
+            // traslo l'oggetto in questione per un valore di velocità * una variabile di tempo che varia a seconda del PC.    
+    }
+
+    void FixedUpdate()
+    {
+       // this.GetComponent<Rigidbody>().MovePosition(this.GetComponent<Rigidbody>().position + vettoreMovimento * speed * Time.deltaTime);
+    }
+}
