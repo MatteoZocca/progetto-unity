ec602cdb77590ca68d78a958c8e49a9ad2deb38a
MatteoZocca <mat.zocca@stud.uniroma3.it>
2020-03-23 22:59:13 +0100

    Direzione Giocatore Corsa V2
    Orientamento giocatore corsa, rotazione di questo sulle 4 direzioni possibili.

--- Terrapiattisti/Assets/Scripts/MovementPlayer.cs
+++ Terrapiattisti/Assets/Scripts/MovementPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,65 +10,117 @@ public class MovementPlayer : MonoBehaviour
     float speed = 100;
     Vector3 vettoreMovimento; // vettore che indica la direzione del movimento
     Transform astronauta;
+    [SerializeField]
     private Quaternion cambiorotazione;
     private Vector3 cambiodirezione;
-    bool girato = false;
+    bool giratoav = true;
+    bool giratosx = false;
+    private bool giratodx = false;
+    bool giratodt = false;
     float inverti = 0;
     [SerializeField]
-    private float rotationspeed = 100;
+    private float rotationspeed = 200;
+    GameObject parentrigidbody;
+    private bool isRotating;
     private void Start()
     {
         astronauta = GameObject.FindGameObjectWithTag("Player").transform;
+        parentrigidbody = GameObject.FindGameObjectWithTag("GameController");
     }
 
     void Update()
     {
+        // MATTEO: se vuoi che l'input non sia "lerpato" e che scatti tipo alla dark souls, usa Input.GetAxisRaw()
         float movementX = Input.GetAxis("Horizontal"); //salvo input asse X
         float movementY = Input.GetAxis("Vertical"); // salvo input asse Y
 
+        /* Comunque, da quello che ho visto in pratica non calcoli la diagonale, per cui quello va solo nelle 4 direzioni
+         * per ora ho messo che deve aspettare che il player aspetti di ruotare per poi fare altre cose
+         * 
+         */
         if (Input.GetKeyDown(KeyCode.W))
         {
-            if (girato)
+            if (!giratoav && !isRotating)
             {
-                girato = false;
+                isRotating = true;
+                giratoav = true;
+                giratodx = false;
+                giratodt = false;
+                giratosx = false;
                 //cambiorotazione = Quaternion.LookRotation(-astronauta.forward, astronauta.up);
                 //StartCoroutine(PerformRotation(cambiorotazione));
                 //astronauta.rotation = Quaternion.Euler(0f, 0f, 0f);
-                astronauta.Rotate(new Vector3(0, 180f));
+                //astronauta.Rotate(new Vector3(0, 180f));
+                cambiorotazione = Quaternion.LookRotation(parentrigidbody.transform.forward, parentrigidbody.transform.up);
+                StartCoroutine(PerformRotation(cambiorotazione, () => { isRotating = false; }));
+                Debug.Log("Forward");
                 inverti = 0;
             }
         }
-
-        else if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            if (!girato) // Se non è girato
+            if (!giratosx && !isRotating)
             {
-                girato = true; // giralo
+                isRotating = true;
+                giratosx = true;
+                giratodt = false;
+                giratoav = false;
+                giratodx = false;
+                cambiorotazione = Quaternion.LookRotation(parentrigidbody.transform.right *-1, parentrigidbody.transform.up);
+                StartCoroutine(PerformRotation(cambiorotazione,() => { isRotating = false; }));
+                Debug.Log("Right");
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            // NANI !? perchè qua non si controlla se è girato ? 
+            if (!giratodx && !isRotating)
+            {
+                isRotating = true;
+                giratosx = false;
+                giratodt = false;
+                giratoav = false;
+                giratodx = true;
+                cambiorotazione = Quaternion.LookRotation(parentrigidbody.transform.right, parentrigidbody.transform.up);
+                StartCoroutine(PerformRotation(cambiorotazione, () => { isRotating = false; }));
+                Debug.Log("Left");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (!giratodt && !isRotating) // Se non è girato
+            {
+                isRotating = true;
+                giratodt = true;
+                giratoav = false;
+                giratosx = false;
+                giratodx = false;
                                //astronauta.transform.rotation = Quaternion.Slerp(astronauta.transform.rotation, cambiorotazione, 1f * rotationspeed * Time.deltaTime);
                                //cambiorotazione = astronauta.rotation * Quaternion.Euler(0, 180f, 0);
-                               //cambiorotazione = Quaternion.LookRotation(-astronauta.forward, astronauta.up);
-                astronauta.Rotate(new Vector3(0, 180f));
-                //StartCoroutine(PerformRotation(cambiorotazione));
-
+                cambiorotazione = Quaternion.LookRotation(-parentrigidbody.transform.forward, parentrigidbody.transform.up);
+                               //astronauta.Rotate(new Vector3(0, 180f));
+                StartCoroutine(PerformRotation(cambiorotazione, () => { isRotating = false; }));
+                Debug.Log("Back");
                 inverti = 0; // e inverti i comandi
             }
         }
-        
-        vettoreMovimento = new Vector3(movementX, 0, movementY); // creo un nuovo vettore di movimento 
-        if(inverti == 1)
+        if (giratoav || giratodt)
+            vettoreMovimento = new Vector3(0, 0, movementY);
+        else
+            vettoreMovimento = new Vector3(movementX, 0, 0);
+        if(inverti == 1f)
             vettoreMovimento.Scale(new Vector3(-1, -1, -1));
         this.transform.Translate(vettoreMovimento * speed * Time.deltaTime);
             // traslo l'oggetto in questione per un valore di velocità * una variabile di tempo che varia a seconda del PC. 
          
     }
 
-    IEnumerator PerformRotation(Quaternion targetRotation)
+    IEnumerator PerformRotation(Quaternion targetRotation, Action onComplete)
     {
-
         float progress = 0f;
         while (progress < 1f)
         {
-
             astronauta.rotation = Quaternion.Slerp(astronauta.rotation, targetRotation, progress);
             progress += Time.deltaTime * rotationspeed;
 
@@ -76,6 +129,7 @@ public class MovementPlayer : MonoBehaviour
                 yield return null;
             }
         }
+        onComplete?.Invoke();
     }
 
 }
--- Terrapiattisti/Assets/Scripts/TouchController.cs
+++ Terrapiattisti/Assets/Scripts/TouchController.cs
@@ -9,15 +9,119 @@ public class TouchController : MonoBehaviour
     private Vector3 vettore;
     [SerializeField] // compare nell'Inspector, anche public lo fa, con la differenza che la rende visibile ad altre classi.
     private float velocita = 5;
+    [SerializeField]
+    private float rotationspeed = 5;
     private Vector3 vettoreMovimento;
     // Update is called once per frame
+    private Quaternion cambiorotazione;
+    private Transform astronauta;
+    private GameObject parentrigidbody;
+    private bool isRotating;
+    bool giratoav = true;
+    bool giratosx = false;
+    bool giratodx = false;
+    bool giratodt = false;
+
+    private void Start()
+    {
+        astronauta = GameObject.FindGameObjectWithTag("Player").transform;
+        parentrigidbody = GameObject.FindGameObjectWithTag("GameController");
+    }
 
     void Update()
     {
         float movementX = joystick.Horizontal; //salvo input asse X
         float movementY = joystick.Vertical; // salvo input asse Y
-        vettoreMovimento = new Vector3(movementX, 0, movementY); // creo un nuovo vettore di movimento 
-        this.transform.Translate(vettoreMovimento * velocita * Time.deltaTime);
+        
+        if(Mathf.Abs(movementX) > Mathf.Abs(movementY))
+        {
+            if(movementX < 0 && !isRotating)
+            {
+                if (!giratosx)
+                {
+                    giratosx = true;
+                    giratodt = false;
+                    giratoav = false;
+                    giratodx = false;
+                    cambiorotazione = Quaternion.LookRotation(parentrigidbody.transform.right * -1, parentrigidbody.transform.up);
+                    StartCoroutine(PerformRotation(cambiorotazione, () => { isRotating = false; }));
+                }
+            }
+            else if(movementX > 0 && !isRotating)
+            {
+                if (!giratodx)
+                {
+                    giratodx = true;
+                    giratodt = false;
+                    giratoav = false;
+                    giratodt = false;
+                    cambiorotazione = Quaternion.LookRotation(parentrigidbody.transform.right, parentrigidbody.transform.up);
+                    StartCoroutine(PerformRotation(cambiorotazione, () => { isRotating = false; }));
+                }
+            }
+
+        }
+        else
+        {
+            if(movementY > 0 && !isRotating)
+            {
+                if (!giratoav)
+                {
+                    giratoav = true;
+                    giratodx = false;
+                    giratodt = false;
+                    giratosx = false;
+                    //cambiorotazione = Quaternion.LookRotation(-astronauta.forward, astronauta.up);
+                    //StartCoroutine(PerformRotation(cambiorotazione));
+                    //astronauta.rotation = Quaternion.Euler(0f, 0f, 0f);
+                    //astronauta.Rotate(new Vector3(0, 180f));
+                    cambiorotazione = Quaternion.LookRotation(parentrigidbody.transform.forward, parentrigidbody.transform.up);
+                    StartCoroutine(PerformRotation(cambiorotazione, () => { isRotating = false; }));
+                }
+            }
+            else if (movementY < 0 && !isRotating)
+            {
+                if (!giratodt) // Se non è girato
+                {
+                    giratodt = true;
+                    giratoav = false;
+                    giratosx = false;
+                    giratodx = false;
+                    //astronauta.transform.rotation = Quaternion.Slerp(astronauta.transform.rotation, cambiorotazione, 1f * rotationspeed * Time.deltaTime);
+                    //cambiorotazione = astronauta.rotation * Quaternion.Euler(0, 180f, 0);
+                    cambiorotazione = Quaternion.LookRotation(-parentrigidbody.transform.forward, parentrigidbody.transform.up);
+                    //astronauta.Rotate(new Vector3(0, 180f));
+                    StartCoroutine(PerformRotation(cambiorotazione, () => { isRotating = false; }));
+                }
+            }
+        }
+        if(Mathf.Abs(movementY) > Mathf.Abs(movementX))
+            vettoreMovimento = new Vector3(0, 0, movementY); // creo un nuovo vettore di movimento 
+        else
+            vettoreMovimento = new Vector3(movementX, 0, 0);
+
+        //this.transform.Translate(vettoreMovimento * speed * Time.deltaTime);
         this.transform.Rotate(0, rjoystick.Horizontal * 2.5f, 0);
+        this.transform.Translate(vettoreMovimento * velocita * Time.deltaTime);
+    }
+
+    
+
+    IEnumerator PerformRotation(Quaternion targetRotation, System.Action onComplete)
+    {
+
+        float progress = 0f;
+        while (progress < 1f)
+        {
+
+            astronauta.rotation = Quaternion.Slerp(astronauta.rotation, targetRotation, progress);
+            progress += Time.deltaTime * rotationspeed;
+
+            if (progress <= 1f)
+            {
+                yield return null;
+            }
+        }
+        onComplete?.Invoke();
     }
 }
