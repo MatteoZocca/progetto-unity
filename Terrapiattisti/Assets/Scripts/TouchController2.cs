740442122456e3490b0a647740a5d8a25b8764bc
MatteoZocca <mat.zocca@stud.uniroma3.it>
2020-04-05 22:34:26 +0200

    Implentazione Nuovo Movimento
    Introduzione del nuovo sistema di movimento con solo analogico sinistro.

--- Terrapiattisti/Assets/Scripts/TouchController2.cs
+++ Terrapiattisti/Assets/Scripts/TouchController2.cs
@@ -6,10 +6,10 @@ public class TouchController2 : MonoBehaviour
 {
     public Joystick ljoystick;
     public Transform astronauta;
-    public float velocita = 5;
+    public float velocity = 5;
     private Vector3 vettoreMovimento;
     private Vector3 forwardIniziale;
-    public float rotaspeed = 5f;
+    public float rotaspeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,19 +23,15 @@ public class TouchController2 : MonoBehaviour
     {
 
         vettoreMovimento = new Vector3(ljoystick.Horizontal, 0f, ljoystick.Vertical);
-        float heading = Mathf.Atan2(ljoystick.Horizontal, ljoystick.Vertical);
-        float angle = Vector3.SignedAngle(astronauta.forward, vettoreMovimento, this.transform.up);
+        Vector3 vettoreRelativoPlayer = transform.TransformDirection(vettoreMovimento); //trasforma il vettore locale in globale
+        float angle = Vector3.SignedAngle(astronauta.forward, vettoreRelativoPlayer, this.transform.up);
         
-        Debug.DrawLine(this.transform.position, vettoreMovimento);
-        //Debug.DrawRay(this.transform.position, vettoreMovimento, Color.red, 0.5f);
-        //astronauta.up = this.transform.up;
-        //astronauta.rotation = Quaternion.LookRotation(vettoreMovimento, this.transform.up);
-        this.transform.Translate(vettoreMovimento * velocita * Time.deltaTime);
-        Vector3 rotazionea = astronauta.rotation.eulerAngles;
+
+        this.transform.Translate(vettoreMovimento * velocity * Time.deltaTime);
 
         if (ljoystick.Horizontal != 0 && ljoystick.Vertical != 0)
         {
-            astronauta.Rotate(new Vector3(0f, angle, 0f) * Time.deltaTime * rotaspeed);
+            astronauta.Rotate(new Vector3(0f, angle, 0f) * Time.deltaTime * rotaspeed, Space.Self);
         }
 
     }
