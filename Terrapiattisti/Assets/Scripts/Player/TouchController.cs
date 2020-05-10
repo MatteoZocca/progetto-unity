73f7e5f91bf07d75119a923bc9cee00d67c31c45
RobertoTafuro <rob.tafuro@stud.uniroma3.it>
2020-05-10 16:29:03 +0200

    Aggiunto controller astronauta

---
+++ Terrapiattisti/Assets/Scripts/Player/TouchController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    public Joystick ljoystick;
+    public Joystick rjoystick;
+
+    public Transform astronauta;
+    private Animator animator;
+
+    public float velocity = 5;
+    public float rotaspeed = 10f;
+
+    private Vector3 vettoreMovimento;
+    private Vector3 forwardIniziale;
+    
+    // Start is called before the first frame update
+    void Start() {
+        if (astronauta == null)
+            astronauta = GameObject.FindGameObjectWithTag("Player").transform.GetChild(1).transform;
+
+        animator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        forwardIniziale = astronauta.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        vettoreMovimento = new Vector3(ljoystick.Horizontal, 0f, ljoystick.Vertical);
+        Vector3 vettoreRelativoPlayer = transform.TransformDirection(vettoreMovimento); //trasforma il vettore locale in globale
+        float angle = Vector3.SignedAngle(astronauta.forward, vettoreRelativoPlayer, this.transform.up);
+
+
+        this.transform.Translate(vettoreMovimento * velocity * Time.deltaTime);
+        this.transform.Rotate(0f, rjoystick.Horizontal * 2f, 0);
+
+        if (vettoreMovimento != Vector3.zero)
+            astronauta.Rotate(new Vector3(0f, angle, 0f) * Time.deltaTime * rotaspeed, Space.Self);
+
+        TriggerAnimations();
+    }
+
+    private void TriggerAnimations() {
+        if (vettoreMovimento.x > 0.7 || vettoreMovimento.x < -0.7 || vettoreMovimento.z > 0.7 || vettoreMovimento.z < -0.7) {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isIdle", false);
+        }
+        else if (vettoreMovimento.x == 0 && vettoreMovimento.z == 0) {
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+        }
+
+        else if (vettoreMovimento.x != 0 && vettoreMovimento.z != 0) {
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
