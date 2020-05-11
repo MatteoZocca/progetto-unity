69ebc9207d6c8161ac90a1e16edc858c34da715a
RobertoTafuro <rob.tafuro@stud.uniroma3.it>
2020-05-11 17:01:47 +0200

    Aggiunta macchina a stati finiti per AI

---
+++ Terrapiattisti/Assets/Scripts/Characters/EnemyAI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+    // Ogni AI possiede questa classe che contiene
+    // gli stati da eseguire
+    private AIBehaviuor brain;
+    // L'oggetto da inseguire
+    private Transform target;
+    // Oggetto che impone il cammino
+    private NavMeshAgent agent;
+    // Posizione random corrente
+    private Vector3 wanderPosition;
+
+    private float wanderSpeed;
+    private float wanderRadius;
+
+    private void Start() {
+        this.wanderRadius = 5;
+        this.wanderSpeed = 5;
+        this.wanderPosition = GetRandomPosition();
+        this.brain = new AIBehaviuor();
+        this.brain.PushState(Wander);
+        this.target = GameObject.FindGameObjectWithTag("Player").transform;
+        this.agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        brain.Update();
+    }
+
+    // Queste funzioni vengono invocate ogni frame
+    private void Wander() {
+        if (Vector3.Distance(transform.position, wanderPosition) < 0.5f)
+            wanderPosition = GetRandomPosition();
+        else
+            agent.SetDestination(wanderPosition);   
+    }
+
+    private void Chase() {
+
+    }
+
+    private Vector3 GetRandomPosition() {
+        Vector3 zone = Random.insideUnitSphere * wanderRadius + transform.position;
+        zone.y = 0;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(zone, out hit, wanderRadius, -1);
+        return new Vector3(hit.position.x, 0, hit.position.z);
+    }
+
+    private bool WaitChance() {
+        float chance = Random.Range(0, 1);
+        return (chance > 0.5) ? true : false;
+    }
+}
--- Terrapiattisti/Assets/Scripts/Player/TouchController.cs
+++ Terrapiattisti/Assets/Scripts/Player/TouchController.cs
@@ -10,7 +10,7 @@ public class TouchController : MonoBehaviour
     public Transform astronauta;
     private Animator animator;
 
-    public float velocity = 5;
+    public float speed = 5;
     public float rotaspeed = 10f;
 
     private Vector3 vettoreMovimento;
@@ -34,7 +34,7 @@ public class TouchController : MonoBehaviour
         float angle = Vector3.SignedAngle(astronauta.forward, vettoreRelativoPlayer, this.transform.up);
 
 
-        this.transform.Translate(vettoreMovimento * velocity * Time.deltaTime);
+        this.transform.Translate(vettoreMovimento * speed * Time.deltaTime);
         this.transform.Rotate(0f, rjoystick.Horizontal * 2f, 0);
 
         if (vettoreMovimento != Vector3.zero)
