a4af4e8b63caef95a2c95d038070fd8f8f3efa45
Matteo Zocca <mat.zocca@stud.uniroma3.it>
2020-07-09 20:08:13 +0200

    Inserimento blocco player placcamento nemico
    Meccanica di gameplay la quale permette di impedire il movimento del giocatore dopo il placcaggio del nemico.

---
+++ Terrapiattisti/Assets/Scripts/Player/blockPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockPlayer : MonoBehaviour
+{
+    public Transform metarig;
+    public float raggioCattura;
+    TouchController touch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        touch = this.GetComponent<TouchController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(metarig.position, raggioCattura);
+        foreach (Collider coll in hitColliders) {
+            if (coll.transform.tag == "mani")
+            {
+                    touch.fermate = true;            
+            }
+        }
+    }
+}
