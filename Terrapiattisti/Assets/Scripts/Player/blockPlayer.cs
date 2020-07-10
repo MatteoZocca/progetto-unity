b2f460428e824c161e39749f8ba8037d01d6ec29
Matteo Zocca <mat.zocca@stud.uniroma3.it>
2020-07-10 18:09:15 +0200

    Inserimento SkillCheck
    Da modificare layout grafico

--- Terrapiattisti/Assets/Scripts/Player/blockPlayer.cs
+++ Terrapiattisti/Assets/Scripts/Player/blockPlayer.cs
@@ -7,21 +7,50 @@ public class blockPlayer : MonoBehaviour
     public Transform metarig;
     public float raggioCattura;
     TouchController touch;
+    public Checker checker;
+    private bool checkerDone;    
     // Start is called before the first frame update
     void Start()
     {
         touch = this.GetComponent<TouchController>();
+        checker.OnSkillCheckDone.AddListener(checkDone);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (checkerDone)
+            return;
+
         Collider[] hitColliders = Physics.OverlapSphere(metarig.position, raggioCattura);
+        
         foreach (Collider coll in hitColliders) {
-            if (coll.transform.tag == "mani")
+            if (coll.transform.tag == "mani" && touch.fermate != true)
             {
-                    touch.fermate = true;            
+                    touch.fermate = true;
+                    checker.gameObject.SetActive(true);
             }
         }
     }
+
+    private void checkDone(bool done)
+    {
+        if (done)
+        {
+            checkerDone = true;
+            checker.gameObject.SetActive(false);
+            touch.fermate = false;
+            StartCoroutine(staiFermo());
+        }
+        //do nothing
+    }
+
+    IEnumerator staiFermo()
+    {
+        yield return new WaitForSeconds(3f);
+        checkerDone = false;
+
+    }
+    
+
 }
---
+++ Terrapiattisti/Assets/Scripts/UI/SkillCheck.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SkillCheck : MonoBehaviour
+{
+    public event Action OnSkillCheckFail, OnSkillCkeckGood, OnSkillCheckGreat;
+
+    [Range(1, 50)]
+    [SerializeField] private int arrowRotationSpeed = 30;
+    [SerializeField] private GameObject arrowRotator, circle, space, pointsUi;
+
+    private float arrowRotation = 0.0f;
+    private bool isSkillCheckOn = false;
+
+    public void StartSkillCheck()
+    {
+        if (!isSkillCheckOn)
+        {
+            Invoke("SkillCheckStarter", 0.6f);
+        }
+    }
+    void SkillCheckStarter()
+    {
+        Vector3 currentCircleRotation = circle.transform.rotation.eulerAngles;
+        circle.transform.rotation = Quaternion.Euler(currentCircleRotation.x, currentCircleRotation.y, UnityEngine.Random.Range(160, 360));
+        SetActiveGameUI(true);
+        isSkillCheckOn = true;
+    }
+    void DoSkillCheck()
+    {
+        // Rotate the arrow and store current rotation angle
+        arrowRotation += Time.deltaTime * arrowRotationSpeed * -10;
+        arrowRotator.transform.Rotate(0, 0, Time.deltaTime * arrowRotationSpeed * -10, Space.World);
+
+        if (Input.GetKeyDown("space"))
+        {
+            // Arrow in good skill check zone
+            if (arrowRotator.transform.rotation.eulerAngles.z >= circle.transform.rotation.eulerAngles.z - 158
+                && arrowRotator.transform.rotation.eulerAngles.z < circle.transform.rotation.eulerAngles.z - 120)
+            {
+                ActivatePointsUI("GoodSC");
+            }
+            // Arrow in great skill check zone
+            else if (arrowRotator.transform.rotation.eulerAngles.z >= circle.transform.rotation.eulerAngles.z - 120
+                && arrowRotator.transform.rotation.eulerAngles.z <= circle.transform.rotation.eulerAngles.z - 110)
+            {
+                ActivatePointsUI("GreatSC");
+            }
+            // Arrow not in skill check zone
+            else
+            {
+                FinishSkillCheck(false);
+            }
+        }
+        // Arrow went full circle
+        if (-arrowRotation >= 360)
+        {
+            FinishSkillCheck(false);
+        }
+
+    }
+    public void FinishSCIfActive()
+    {
+        if (isSkillCheckOn)
+        {
+            FinishSkillCheck(false);
+        }
+    }
+    void FinishSkillCheck(bool result)
+    {
+        isSkillCheckOn = false;
+        arrowRotation = 0.0f;
+        arrowRotator.transform.rotation = Quaternion.Euler(Vector3.zero);
+        SetActiveGameUI(false);
+     
+     
+        if (!result)
+        {
+            OnSkillCheckFail();
+        }
+        else
+        {
+            OnSkillCkeckGood();
+        }
+    }
+    void ActivatePointsUI(string type)
+    {
+        foreach (Transform child in pointsUi.transform)
+        {
+            child.gameObject.SetActive(child.name == type);
+        }
+        pointsUi.SetActive(true);
+        Invoke("DeactivatePointsUI", 3);
+    }
+    void DeactivatePointsUI()
+    {
+        pointsUi.SetActive(false);
+    }
+    void SetActiveGameUI(bool isActive)
+    {
+        arrowRotator.SetActive(isActive);
+        circle.SetActive(isActive);
+        space.SetActive(isActive);
+    }
+ 
+    void Update()
+    {
+        if (isSkillCheckOn)
+        {
+            DoSkillCheck();
+        }
+    }
+}
