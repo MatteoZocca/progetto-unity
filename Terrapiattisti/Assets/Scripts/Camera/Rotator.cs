6a65469496bec3843fb7c61e34ae2a9cf4b1fa97
RobertoTafuro <rob.tafuro@stud.uniroma3.it>
2020-07-11 22:58:12 +0200

    Modifiche schermata menu

---
+++ Terrapiattisti/Assets/Scripts/Camera/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    [SerializeField] private Transform pivot;
+    [SerializeField] private float rotationSpeed;
+
+    // Update is called once per frame
+    void Update() {
+        pivot.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
