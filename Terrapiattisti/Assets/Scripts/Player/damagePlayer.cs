81548c2cea9a3fc87ac6a1a3dfd6d18c551317c0
Matteo Zocca <mat.zocca@stud.uniroma3.it>
2020-07-13 00:31:26 +0200

    Inserimento danno e barravita

---
+++ Terrapiattisti/Assets/Scripts/Player/damagePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class damagePlayer : MonoBehaviour
+{
+    public Transform testa;
+    public float raggioCattura;
+    private GameObject player;
+    private GameObject lifebar;
+    public float danno;
+    TouchController touch;
+    private Slider slider;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        lifebar = GameObject.Find("lifebar");
+        touch = player.GetComponent<TouchController>();
+        slider = lifebar.GetComponent<Slider>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(testa.position, raggioCattura);
+        foreach (Collider coll in hitColliders)
+        {
+            if (coll.transform.tag == "cartello")
+            {
+                Debug.Log("Danno");
+                slider.value -= danno;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+     //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
+     Gizmos.DrawWireSphere(testa.position, raggioCattura);
+    }
+}
