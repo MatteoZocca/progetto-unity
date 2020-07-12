c280a146c966651ebffe72b56b8d98c7e545115f
Viorel <viorelscurelnic@outlook.it>
2020-07-12 22:51:18 +0200

    chargeBAR
    chargebar per il tempo di caricamento del razzo

---
+++ countdown/Assets/ChargingBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargingBar : MonoBehaviour
+{
+    public bool razzoDisponibile = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Slider>().value=0;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<Slider>().value == 1)
+            razzoDisponibile = true;
+
+        this.GetComponent<Slider>().value += Time.deltaTime/100;
+
+    }
+}
