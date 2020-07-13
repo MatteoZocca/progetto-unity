9abec92653fd5b706fdab863fd7bd9e4a06e9ce8
Viorel <viorelscurelnic@outlook.it>
2020-07-13 13:08:25 +0200

    Fixes

--- Terrapiattisti/Assets/Scripts/UI/SceneChanger.cs
+++ Terrapiattisti/Assets/Scripts/UI/SceneChanger.cs
@@ -6,5 +6,6 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour {
     public void ChangeScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1;
     }
 }
