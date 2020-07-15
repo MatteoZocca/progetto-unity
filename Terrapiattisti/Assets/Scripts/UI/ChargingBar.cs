29f783622f4d45cfa96a6a393fa77f6a6752f27e
Viorel <viorelscurelnic@outlook.it>
2020-07-15 11:48:39 +0200

    Ready!

--- Terrapiattisti/Assets/Scripts/UI/ChargingBar.cs
+++ Terrapiattisti/Assets/Scripts/UI/ChargingBar.cs
@@ -8,12 +8,15 @@ public class ChargingBar : MonoBehaviour
     public bool razzoDisponibile = false;
     public float difficulty;
     private float difficultyPercentage;
+    private bool nonattivato;
+    public GameObject readyPanel;
     // Start is called before the first frame update
     void Start()
     {
         this.GetComponent<Slider>().value = 0;
         difficulty = 1;
         difficultyPercentage = 50 * difficulty;
+        nonattivato = true;
     }
 
     // Update is called once per frame
@@ -22,7 +25,16 @@ public class ChargingBar : MonoBehaviour
         if (this.GetComponent<Slider>().value == this.GetComponent<Slider>().maxValue)
             razzoDisponibile = true;
 
+        if (razzoDisponibile && nonattivato)//display ready se razzo prontu 
+        {
+            readyPanel.SetActive(true);
+           
+           nonattivato = false;
+        }
+
         this.GetComponent<Slider>().value += Time.deltaTime / difficultyPercentage;
+
+
 
     }
 }
