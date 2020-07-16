dbe78c884334a5d501284a513edbeff9efac25f3
RobertoTafuro <rob.tafuro@stud.uniroma3.it>
2020-07-16 13:54:06 +0200

    Toggle musica/effetti sonori

---
+++ Terrapiattisti/Assets/Scripts/UI/SoundEffectsToggler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundEffectsToggler : MonoBehaviour
+{
+    [SerializeField] private GameObject[] objectsWithSoundEffects;
+    private bool status;
+    // Start is called before the first frame update
+    void Start() {
+        objectsWithSoundEffects = GetObjectsWithSound();
+        status = this.GetComponent<Toggle>().isOn;
+    }
+
+    private GameObject[] GetObjectsWithSound() {
+        GameObject[] objects = GameObject.FindObjectsOfType<GameObject>();
+        for (int i = 0; i < objects.Length; i++) {
+            if (objects[i].GetComponent<AudioSource>() == null)
+                objects[i] = null;
+        }
+        return objects;
+    }
+
+    public void ChangeStatus() {
+        for (int i = 0; i < objectsWithSoundEffects.Length; i++) {
+            if (objectsWithSoundEffects[i] != null) {
+                objectsWithSoundEffects[i].GetComponent<AudioSource>().mute = status;
+            }
+        }
+    }
+}
